077a7790be8611ccacb0045c5d6e19fa6ac3a7f4
zrideaux <zacharyrideaux@gmail.com>
2020-06-23 21:02:54 +0000

    Add function to display a tab by default on page load.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -47,8 +47,16 @@ function openTab(event, tabName) {
     }
     
     // Add the open class to the clicked tab
+    console.log('tabName', tabName);
     buttonClicked = document.getElementById(tabName);
+    console.log('buttonClicked', buttonClicked);
     buttonClicked.style.display = 'inline';
-    event.currentTarget.className += ' open';
-    
+    event.currentTarget.className += ' open';   
+}
+
+/**
+ * Display specified tab when called.
+ */
+function defaultTab() {
+    document.getElementById('default-tab').click();
 }
