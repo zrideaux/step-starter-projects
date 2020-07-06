fa57cf9caf372f7abac62b56eeddfff3e6c041b1
zrideaux <zacharyrideaux@gmail.com>
2020-07-06 16:32:00 +0000

    Update commentsJson to be private variable.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -30,10 +30,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+/** Servlet that handles comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  String commentsJson = new String();
+  private String commentsJson = new String();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
